cc3658444105061dbbd50924e0bc862efe1f60c8
Maelys LE COZ <maelys.lecoz@gmail.com>
2020-09-15 22:39:28 +0200

    bcrypt

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -2,13 +2,32 @@ const express = require("express")
 const routes = express.Router()
 const db = require("../database/db.js")
 const mysql = require("mysql2")
+const bcrypt = require("bcrypt")
 
-routes.post("/sign-up", function(req, res) {
+let getHashedPassword = (password) => {
+    return new Promise(function(resolve){
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(password, salt, function(err, hash) {
+                resolve(hash)
+            });
+        });        
+    })
+}
+
+let comparePassword = (passwordInput, passwordDB) => {
+    return new Promise(function(resolve) {
+        bcrypt.compare(passwordInput, passwordDB, function(err, res) {
+            resolve(res)
+        });
+    })    
+}
+
+routes.post("/sign-up", async function(req, res) {
     let post = {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-    }
+        password: await getHashedPassword(req.body.password)
+    }    
 
     db.query("INSERT INTO users SET ?", post, function(err, result) {
         if (err) {
@@ -26,7 +45,7 @@ routes.post("/sign-in", function(req, res) {
         password: req.body.password
     }
 
-    db.query("SELECT *  FROM `users` WHERE `email` = " + mysql.escape(post.email) + "AND `password` =" + mysql.escape(post.password), function(err, result) {
+    db.query("SELECT *  FROM `users` WHERE `email` = " + mysql.escape(post.email), async function(err, results) {
         if (err) {
             res.status(500)
             res.send()
@@ -34,12 +53,14 @@ routes.post("/sign-in", function(req, res) {
         }
 
         res.status(200)
-
-        if (result.length) {            
-            res.send("You are authenticated!")
-        } else {
-            res.send("Authentication incorrect")
+        
+        for (let user of results) {
+            if (await comparePassword(req.body.password, user.password)) {
+                return res.send("You are athenticated!")
+            }
         }
+
+        res.send("Authentication failed")
     })   
 })
 
